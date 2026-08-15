49c90fd561e60c89faccf28bb2e5b6d2c50de99d
Aleksey <19895415+Jackhammer88@users.noreply.github.com>
2026-08-15 17:52:19 +0300

    V3

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 import { dotnet } from './_framework/dotnet.js'
 
+const splash = document.querySelector('.app-splash');
+const host = document.getElementById('out');
+if (splash && host) {
+    const hide = () => {
+        if (!host.querySelector('canvas')) return false;
+        splash.remove();
+        return true;
+    };
+
+    if (!hide()) {
+        const observer = new MutationObserver(() => {
+            if (hide()) observer.disconnect();
+        });
+        observer.observe(host, { childList: true });
+    }
+}
+
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
 
